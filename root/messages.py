bcc82164c1a331398f82ac60eea9b0bc3da30ee9
sahaynitin4telly <87236420+sahaynitin4telly@users.noreply.github.com>
2021-07-14 14:35:23 +0530

    Update messages.py

--- root/messages.py
+++ root/messages.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = "**I'm a Rename and Convert Bot\nJust send me any media to change file name.\nUse /help command for more details **"
+    START_TEXT = "****"
 ######################
     HELP_USER = """**>>Send File/Video\n>>Select desired Option\n>>And Done wait for it to process files**"""
     DOWNLOAD_MSG = "**Downloading **⏬"
